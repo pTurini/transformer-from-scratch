6326abd2ac025da7d316156af9f00ea1c0786568
Pedro Turini <pedro.turini@usp.br>
2025-01-16 17:58:36 -0300

    adds dropouts and layer norm, and increments parameters

--- bigram_w_attention.py
+++ bigram_w_attention.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_layer = 6
+n_head = 6
+dropout = 0.2
 # -----------------------------
 
 torch.manual_seed(1337)
@@ -67,6 +70,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) #buffer
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         B,T,C = x.shape
         k = self.key(x)
@@ -75,6 +80,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1) *C**-0.5
         wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) #decoder block
         wei = F.softmax(wei, dim =-1)
+        wei = self.dropout(wei)
         v= self.value(x)
         out = wei @ v
         return out
@@ -84,9 +90,11 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)]) #instantiates multiple head layers
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim = -1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
 
 class FeedForward(nn.Module):
@@ -94,7 +102,9 @@ class FeedForward(nn.Module):
         super().__init__()
         self.net = nn.Sequential(nn.Linear(n_embd, 4 * n_embd), 
                                  nn.ReLU(),
-                                 nn.Linear(4 * n_embd,n_embd),) #creates Linear NN and RELu activation in sequence
+                                 nn.Linear(4 * n_embd,n_embd),
+                                 nn.Dropout(dropout),
+                                 ) #creates Linear NN and RELu activation in sequence
     
     def forward(self,x):
         return self.net(x)
@@ -107,10 +117,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x) #skip connections
-        x = x + self.ffwd(x) #skip connections
+        x = x + self.sa(self.ln1(x)) #skip connections, layer norm
+        x = x + self.ffwd(self.ln2(x)) #skip connections, layer norm
         return x
 
 
@@ -122,10 +134,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) #32 dimensional embeddings
         self.position_embedding_table = nn.Embedding(block_size,n_embd) #each token receives also a position embedding
-        self.blocks = nn.Sequential(Block(n_embd, n_head=4),
-                                     Block(n_embd, n_head=4),
-                                     Block(n_embd, n_head=4),
-                                     Block(n_embd, n_head=4),)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size) #linear layer, 
 
     def forward(self, idx, targets=None):
@@ -138,6 +148,7 @@ class BigramLanguageModel(nn.Module):
         #x = self.sa_heads(x) #(B,T,C)
         #x = self.ffwd(x) # (B,T,C)
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B,T, vocab_size) This is a decoder
 
         if targets is None:
